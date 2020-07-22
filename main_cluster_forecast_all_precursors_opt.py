8943295ad8dfd6689ad950f10d1c359cc1c66880
Sonja Totz <sonja.totz@gmail.com>
2020-07-22 16:43:49 -0400

    updated main_cluster_forecast_precursors_opt.py

--- main_cluster_forecast_all_precursors_opt.py
+++ main_cluster_forecast_all_precursors_opt.py
@@ -12,9 +12,10 @@ from classes.Forecast import Forecast
 from classes.ExportVarPlot import ExportVarPlot
 from classes.ClusteringParser import ClusteringParser
 from classes.Config import Config
-from scipy.optimize import shgo
+from scipy.optimize import shgo, dual_annealing
 import json
 import math
+from scipy import stats
 
 def lat_range(x):
     if (x[1] < 0 and x[0] > 0):
@@ -28,8 +29,8 @@ def lon_range(x):
     return x[3] - x[2] - 10  # >=0
 
 
-
-def train_test_split_pred(predictand, precursors, data_range, forecast_predictands: list, train_size=0.66, random_state=2019,):
+def train_test_split_pred(predictand, precursors, data_range, forecast_predictands: list, train_size=0.66,
+                          random_state=2019, ):
     """
     calculate number of train and test points
     : param predictand: obect of the class Predictand
@@ -61,8 +62,7 @@ def train_test_split_pred(predictand, precursors, data_range, forecast_predictan
     return y_train, X_train, y_test, X_test
 
 
-
-def train_test_split_prec(precursors, data_range, forecast_predictands: list, train_size=0.66, random_state=2019,):
+def train_test_split_prec(precursors, data_range, forecast_predictands: list, train_size=0.66, random_state=2019, ):
     """
     calculate number of train and test points
     : param predictand: obect of the class Predictand
@@ -99,6 +99,10 @@ def main(cl_parser: ClusteringParser, cl_config: dict):
     output_label = cl_parser.arguments['outputlabel']
     output_path = cl_parser.arguments['outputpath']
     data_range = cl_parser.arguments['datarange']
+    logger.info(inifile)
+    logger.info(output_path)
+    logger.info(output_label)
+    logger.info(data_range)
     predictand = Predictand(inifile, output_path, output_label, cl_config)
     dict_skills_pattern = {}
 
@@ -111,20 +115,24 @@ def main(cl_parser: ClusteringParser, cl_config: dict):
     # load precursors
     precursors = Precursors(inifile, output_path, output_label, cl_config)
     forecast_precursors = cl_parser.arguments['forecast_precursors']
+    logger.info(forecast_precursors)
 
     y_train, X_train, y_test, X_test = train_test_split_pred(predictand, precursors, data_range, forecast_precursors)
     # Calculate clusters of precursors for var, by removing one year
     predictand.calculate_clusters_from_test_data(y_train, forecast.method_name, forecast.k)
 
     def skill(x, info):
+        if lat_range(x) < 0 or lon_range(x) < 0:
+            return 1
+
         cut_area_opt = x
+        # cut area  and normalize data accordingly
         precursors.set_area_composite_opt(forecast_precursors[0], cut_area_opt)
         # Create train and test dataset with an 66:33 split
         # noinspection PyPep8Naming
         X_train, X_test = train_test_split_prec(precursors, data_range, forecast_precursors)
-        # Calculate clusters of precursors for var, by removing one year
         # Calculate composites
-        precursors.get_composites_data_1d_train_test(X_train, predictand.f, forecast.k, forecast.method_name,
+        precursors.get_composites_data_1d_train_test(X_train, X_test, predictand.f, forecast.k, forecast.method_name,
                                                      predictand.var)
 
         # Calculate forecast_nn for all years
@@ -134,43 +142,67 @@ def main(cl_parser: ClusteringParser, cl_config: dict):
         forecast_data = np.zeros((len(y_test[f"{predictand.var}"]),
                                   predictand.dict_pred_1D[f"{predictand.var}"].shape[1]))
 
-
-        logger.info(forecast_precursors)
-
+        # logger.info(forecast_precursors)
+        skills_score_predictor = np.zeros(len(y_test[predictand.var]))
+        ax_predictor = np.zeros(len(y_test[predictand.var]))
+        corr_predictor = np.zeros(len(y_test[predictand.var]))
         for year in range(len(y_test[predictand.var])):  # len(y_test[predictand.var])):
             # print(year)
+            if math.isnan(np.nanmean(precursors.dict_composites[forecast_precursors[0]][0])):
+                return 1
             forecast_temp = forecast.prediction(predictand.clusters, precursors.dict_composites, X_test, year)
             # Assign forecast_nn data to array
             forecast_data[year] = forecast_temp
+            a_xm = np.std(forecast_data[year])
+            a_xo = np.std(y_test[f"{predictand.var}"][year])
 
+            ax = a_xm / a_xo
+            corr = stats.pearsonr(y_test[f"{predictand.var}"][year], forecast_data[year])[0]
+            skills_score_predictor[year] = (4*(1 + corr)) / ( 2 * (ax + 1/ax)**2) # ((4*(1 + corr)**4) / ( 16 * (ax + 1/ax)**2))
+            ax_predictor[year] = ax
+            corr_predictor[year] = corr
+            # logger.info(
+            #     f"{a_xo} {a_xm} {x[2]:4f} {corr} {skills_score_predictor[year] }")
 
         # Calculate time correlation for each point
         time_correlation, significance = forecast.calculate_time_correlation_all_times(
             np.array(y_test[f"{predictand.var}"]), forecast_data)
         # display information
         # display information
+        skills_score_mena = np.nanmean(skills_score_predictor)
+        ax_mena = np.nanmean(ax_predictor)
+        corr_mena = np.nanmean(corr_predictor)
         time_correlation_mean = np.nanmean(time_correlation)
-        if info['Nfeval'] % 2 == 0:
-            logger.info(f"{x[0]:4f} {x[1]:4f} {x[2]:4f} {x[3]:4f} {time_correlation_mean}")
+        # if info['best_values'] <= time_correlation_mean:
+        #     info['best_values'] = time_correlation_mean
+        if info['best_values'] <= skills_score_mena:
+            info['best_values'] = skills_score_mena
+            logger.info(f"{x[0]:4f} {x[1]:4f} {x[2]:4f} {x[3]:4f} {time_correlation_mean:4f} {skills_score_mena:4f} "
+                        f"{corr_mena:4f} {ax_mena:4f} {info['Nfeval']}")
         info['Nfeval'] += 1
-        if math.isnan(time_correlation_mean):
-            return 1.
-        else:
-            return -time_correlation_mean
-
+        # if math.isnan(time_correlation_mean):
+        #     return 1.
+        # else:
+        #     return -time_correlation_mean
+        return 1 - skills_score_mena
     cons = ({'type': 'ineq', 'fun': lat_range},
             {'type': 'ineq', 'fun': lon_range},)
     var = forecast_precursors[0]
-    bounds = [(precursors.lat_min[var],precursors.lat_max[var] - 10),
+    bounds = [(precursors.lat_min[var], precursors.lat_max[var] - 10),
               (precursors.lat_min[var] + 10, precursors.lat_max[var]),
               (precursors.lon_min[var], precursors.lon_max[var] - 10),
-              (precursors.lon_min[var] + 10,precursors.lon_max[var])]
-    res = shgo(skill, bounds, args=({'Nfeval':0},), iters=3, constraints=cons)
+              (precursors.lon_min[var] + 10, precursors.lon_max[var])]
+    # ~ res = shgo(skill, bounds, args=({'Nfeval':0},), iters=10, constraints=cons)
+    # res = dual_annealing(skill, bounds, args=({'Nfeval': 0, 'best_values': 0},), maxiter=5000)
+    # 2020-07-18 19:12:16,756 - __main__ - INFO - 27.500000 82.500000 43.750000 316.250000 0.29794461371540987 0.17793906843190135 261
+    #  2020-07-19 16:18:40,972 - __main__ - INFO - 50.000000 67.500000 175.000000 316.250000 0.284150 0.502431 0.259510 0.948210 868
+    res = shgo(skill, bounds, args=({'Nfeval': 0, 'best_values': 0},), constraints=cons, iters=5000)
     print(res)
 
 
 if __name__ == '__main__':
     import logging.config
+
     parser = ClusteringParser()
     # logs / log_
     # {sys.argv[2]}.log
@@ -180,4 +212,4 @@ if __name__ == '__main__':
     # read config log file from classes.Config
     logging.config.dictConfig(config.config_dict)
     logger.info("Start clustering program")
-    main(parser,  config.config_dict)
+    main(parser, config.config_dict)
